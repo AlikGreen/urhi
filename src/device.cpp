387dbcf288435919042b94d50c4d854283c67da4
Alik-Green <alikgreen@proton.me>
2025-08-25 18:30:28 +0100

    i am trying to touch type that took me like 10 seconds i am very slow rn anyway. maybe fixed it

--- src/device.cpp
+++ src/device.cpp
@@ -14,6 +14,105 @@
 #include "enums/shaderType.h"
 #include "util/string.h"
 
+TBuiltInResource DefaultTBuiltInResource()
+{
+    TBuiltInResource R = {};
+    R.maxLights = 32;
+    R.maxClipPlanes = 6;
+    R.maxTextureUnits = 32;
+    R.maxTextureCoords = 32;
+    R.maxVertexAttribs = 64;
+    R.maxVertexUniformComponents = 4096;
+    R.maxVaryingFloats = 64;
+    R.maxVertexTextureImageUnits = 32;
+    R.maxCombinedTextureImageUnits = 80;
+    R.maxTextureImageUnits = 32;
+    R.maxFragmentUniformComponents = 4096;
+    R.maxDrawBuffers = 32;
+    R.maxVertexUniformVectors = 128;
+    R.maxVaryingVectors = 8;
+    R.maxFragmentUniformVectors = 16;
+    R.maxVertexOutputVectors = 16;
+    R.maxFragmentInputVectors = 15;
+    R.minProgramTexelOffset = -8;
+    R.maxProgramTexelOffset = 7;
+    R.maxClipDistances = 8;
+    R.maxComputeWorkGroupCountX = 65535;
+    R.maxComputeWorkGroupCountY = 65535;
+    R.maxComputeWorkGroupCountZ = 65535;
+    R.maxComputeWorkGroupSizeX = 1024;
+    R.maxComputeWorkGroupSizeY = 1024;
+    R.maxComputeWorkGroupSizeZ = 64;
+    R.maxComputeUniformComponents = 1024;
+    R.maxComputeTextureImageUnits = 16;
+    R.maxComputeImageUniforms = 8;
+    R.maxComputeAtomicCounters = 8;
+    R.maxComputeAtomicCounterBuffers = 1;
+    R.maxVaryingComponents = 60;
+    R.maxVertexOutputComponents = 64;
+    R.maxGeometryInputComponents = 64;
+    R.maxGeometryOutputComponents = 128;
+    R.maxFragmentInputComponents = 128;
+    R.maxImageUnits = 8;
+    R.maxCombinedImageUnitsAndFragmentOutputs = 8;
+    R.maxCombinedShaderOutputResources = 8;
+    R.maxImageSamples = 0;
+    R.maxVertexImageUniforms = 0;
+    R.maxTessControlImageUniforms = 0;
+    R.maxTessEvaluationImageUniforms = 0;
+    R.maxGeometryImageUniforms = 0;
+    R.maxFragmentImageUniforms = 8;
+    R.maxCombinedImageUniforms = 8;
+    R.maxGeometryTextureImageUnits = 16;
+    R.maxGeometryOutputVertices = 256;
+    R.maxGeometryTotalOutputComponents = 1024;
+    R.maxGeometryUniformComponents = 1024;
+    R.maxGeometryVaryingComponents = 64;
+    R.maxTessControlInputComponents = 128;
+    R.maxTessControlOutputComponents = 128;
+    R.maxTessControlTextureImageUnits = 16;
+    R.maxTessControlUniformComponents = 1024;
+    R.maxTessControlTotalOutputComponents = 4096;
+    R.maxTessEvaluationInputComponents = 128;
+    R.maxTessEvaluationOutputComponents = 128;
+    R.maxTessEvaluationTextureImageUnits = 16;
+    R.maxTessEvaluationUniformComponents = 1024;
+    R.maxTessPatchComponents = 120;
+    R.maxPatchVertices = 32;
+    R.maxTessGenLevel = 64;
+    R.maxViewports = 16;
+    R.maxVertexAtomicCounters = 0;
+    R.maxTessControlAtomicCounters = 0;
+    R.maxTessEvaluationAtomicCounters = 0;
+    R.maxGeometryAtomicCounters = 0;
+    R.maxFragmentAtomicCounters = 8;
+    R.maxCombinedAtomicCounters = 8;
+    R.maxAtomicCounterBindings = 1;
+    R.maxVertexAtomicCounterBuffers = 0;
+    R.maxTessControlAtomicCounterBuffers = 0;
+    R.maxTessEvaluationAtomicCounterBuffers = 0;
+    R.maxGeometryAtomicCounterBuffers = 0;
+    R.maxFragmentAtomicCounterBuffers = 1;
+    R.maxCombinedAtomicCounterBuffers = 1;
+    R.maxAtomicCounterBufferSize = 16384;
+    R.maxTransformFeedbackBuffers = 4;
+    R.maxTransformFeedbackInterleavedComponents = 64;
+    R.maxCullDistances = 8;
+    R.maxCombinedClipAndCullDistances = 8;
+    R.maxSamples = 4;
+    // Limits (booleans)
+    R.limits.nonInductiveForLoops = true;
+    R.limits.whileLoops = true;
+    R.limits.doWhileLoops = true;
+    R.limits.generalUniformIndexing = true;
+    R.limits.generalAttributeMatrixVectorIndexing = true;
+    R.limits.generalVaryingIndexing = true;
+    R.limits.generalSamplerIndexing = true;
+    R.limits.generalVariableIndexing = true;
+    R.limits.generalConstantMatrixVectorIndexing = true;
+    return R;
+}
+
 namespace NRHI
 {
     std::vector<uint32_t> compileToSpirv(const std::string& source_name,
@@ -42,7 +141,7 @@ namespace NRHI
         shader.setEntryPoint("main");
 
         // Set up resources (using default limits)
-        const TBuiltInResource* resources = GetDefaultResources();
+        const TBuiltInResource resources = DefaultTBuiltInResource();
 
         // Parse the shader
         constexpr auto messages = static_cast<EShMessages>(EShMsgSpvRules | EShMsgVulkanRules);
