864c4068bdc2435175f866dfcae325f261244a90
BHASVIC-AlikGreen25 <alik.green25@students.bhasvic.ac.uk>
2025-12-09 18:12:11 +0000

    should have fixed imconfig

--- backends/imgui/imGuiConfig.h
+++ backends/imgui/imGuiConfig.h
@@ -1,4 +1,5 @@
 #pragma once
+#include "imGuiController.h"
 
 namespace Neon::RHI
 {
@@ -10,18 +11,33 @@ namespace Neon::RHI
         TextureView *view;
         Sampler *sampler;
 
+        ImGuiImage()
+                : view(nullptr),
+                  sampler(nullptr) { }
+
         ImGuiImage(TextureView* v)
                 : view(v),
-                  sampler(nullptr)
-        {
-        }
+                  sampler(nullptr) { }
 
         ImGuiImage(TextureView* v, Sampler* s)
             : view(v),
-              sampler(s)
+              sampler(s) { }
+
+        bool operator==(const ImGuiImage& other) const
         {
+            return view == other.view && sampler == other.sampler;
+        }
+
+        bool operator!=(const ImGuiImage& a, const ImGuiImage& b) const
+        {
+            return a != b;
         }
     };
 }
 
 #define ImTextureID Neon::RHI::ImGuiImage
+
+#define ImTextureID_Invalid ImTextureID()
+#define ImTextureID_Compare(lhs, rhs) ((lhs) == (rhs))
+#define ImTextureID_IsNil(id)         ((id).view == nullptr)
+#define ImTextureID_IsValid(id)       (!ImTextureID_IsNil(id))
