886b88abcc41e4b06e582c7c6ccd869858b6eaa6
Alik Green <alikgreen8@gmail.com>
2026-02-25 22:54:58 +0000

    added ReadbackRequest not implemented in vk backend yet (i just like commiting things)

---
+++ src/readbackRequest.h
@@ -0,0 +1,34 @@
+#pragma once
+#include <cstdint>
+#include <span>
+
+namespace urhi
+{
+class ReadbackRequest
+{
+public:
+    ReadbackRequest() = default;
+    virtual ~ReadbackRequest() = default;
+
+    ReadbackRequest(const ReadbackRequest&) = delete;
+    ReadbackRequest& operator= (const ReadbackRequest&) = delete;
+
+    virtual bool isReady() const = 0;
+    virtual void wait() const = 0;
+
+    virtual const void* data() const = 0;
+    virtual size_t size() const = 0;
+
+    template<typename T>
+    std::span<const T> as() const
+    {
+        return { static_cast<const T*>(data()), size() / sizeof(T) };
+    }
+
+    template<typename T>
+    T at(const size_t offset) const
+    {
+        return *reinterpret_cast<const T*>(static_cast<const uint8_t*>(data()) + offset);
+    }
+};
+}
